e000623c8701a538b1c559277f8227e44d3ce283
Ravivarman M <ravivarmanm@outlook.com>
2022-12-20 16:04:13 +0530

    data

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,14 +1,23 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Datas = require("./Data.json");
+const baseUrl = require("./Data.json");
+console.log(baseUrl);
 const Data = () => {
-  const [data, setData] = useState("");
-  return (
-    <div>
-      {Datas.map((items) => (
-        <p key={items.id}>{items.userid}</p>
-      ))}
-    </div>
-  );
+  const [data, setData] = useState({});
+  useEffect(() => {
+    axios.get(baseUrl).then((response) => {
+      setData(response.data);
+    });
+  }, []);
+  if (!data)
+    return (
+      <div>
+        {data.map((item) => (
+          <p key={item.id}>{item.userId}</p>
+        ))}
+      </div>
+    );
 };
+
 export default Data;
